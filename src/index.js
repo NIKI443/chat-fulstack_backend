0b8cccf2e5b545085628f1483d1e09c13aa76e57
NIKI443 <140906614+NIKI443@users.noreply.github.com>
2025-02-21 21:24:29 +0300

    fixed cors

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use('/uploads/images/profile', express.static('src/uploads/images/profile'))
 app.use('/uploads/images/massage', express.static('src/uploads/images/massage'))
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
+		origin: process.env.CORS_ORIGIN_SITE || 'http://localhost:5173',
 		credentials: true,
 	})
 )
